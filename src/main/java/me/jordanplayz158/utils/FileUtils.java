d1086a6174d180e7b27a831394b6e7236dd62e61
Jordan Adams <jordan@jordanplayz158.me>
2021-01-04 19:58:11 -0500

    Update 2.1.2: Combined copyFile.java and downloadFile.java into FileUtils.java, changed variable requirements from Strings to Files as it just makes more sense to ask for files than strings and then converting them to files inside the function.

---
+++ src/main/java/me/jordanplayz158/utils/FileUtils.java
@@ -0,0 +1,37 @@
+package me.jordanplayz158.utils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+public class FileUtils {
+    // Another function I have made for other projects that I put into this one for a simple file copying function
+    public static void copyFile(File internalName, File externalName) throws IOException {
+        // Grabs the file from within the jar to be copied outside of the jar
+        InputStream fileSrc = Thread.currentThread().getContextClassLoader().getResourceAsStream(internalName.getPath());
+
+        // Checks if the file exists so it doesn't overwrite existing settings each time it is run
+        if(externalName.createNewFile()) {
+            assert fileSrc != null;
+            Files.copy(fileSrc, externalName.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
+
+    // Function I made to easily download and save files
+    public static void downloadFile(URL url, File filename) throws IOException {
+        // Opens/reads the url
+        ReadableByteChannel readableByteChannel = Channels.newChannel(url.openStream());
+
+        // Tells the function what to log the downloaded file to (which in this case is the String defined when calling the function, filename)
+        FileOutputStream fileOutputStream = new FileOutputStream(filename);
+
+        // Writes the contents of the readableByteChannel to the file
+        fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+    }
+}
