7b6a93737c16e5117d8057aa746ac9587bc303b5
Jordan Adams <jordan@jordanplayz158.me>
2021-01-08 21:03:50 -0500

    Update 2.1.3: Added FileUtils#copyFile(java.io.File name) If the filename is the same for both internal and external file then you can use this new method to only put the file once.

--- src/main/java/me/jordanplayz158/utils/FileUtils.java
+++ src/main/java/me/jordanplayz158/utils/FileUtils.java
@@ -1,5 +1,7 @@
 package me.jordanplayz158.utils;
 
+import com.sun.istack.internal.NotNull;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -12,7 +14,7 @@ import java.nio.file.StandardCopyOption;
 
 public class FileUtils {
     // Another function I have made for other projects that I put into this one for a simple file copying function
-    public static void copyFile(File internalName, File externalName) throws IOException {
+    public static void copyFile(@NotNull File internalName, @NotNull File externalName) throws IOException {
         // Grabs the file from within the jar to be copied outside of the jar
         InputStream fileSrc = Thread.currentThread().getContextClassLoader().getResourceAsStream(internalName.getPath());
 
@@ -23,8 +25,19 @@ public class FileUtils {
         }
     }
 
+    public static void copyFile(@NotNull File name) throws IOException {
+        // Grabs the file from within the jar to be copied outside of the jar
+        InputStream fileSrc = Thread.currentThread().getContextClassLoader().getResourceAsStream(name.getPath());
+
+        // Checks if the file exists so it doesn't overwrite existing settings each time it is run
+        if(name.createNewFile()) {
+            assert fileSrc != null;
+            Files.copy(fileSrc, name.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
+
     // Function I made to easily download and save files
-    public static void downloadFile(URL url, File filename) throws IOException {
+    public static void downloadFile(@NotNull URL url, @NotNull File filename) throws IOException {
         // Opens/reads the url
         ReadableByteChannel readableByteChannel = Channels.newChannel(url.openStream());
 
